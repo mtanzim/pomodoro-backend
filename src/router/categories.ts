17e4dc14217b6275f4310c0bbe38120319c4dd46
mtanzim <mtanzim@gmail.com>
2019-12-23 02:10:05 +0100

    refactor routers

--- src/router/categories.ts
+++ src/router/categories.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { Categories } from "../entity/Categories";
 import { GenericController } from "../controller/GenericController";
-
-const router = express.Router();
+import { Categories } from "../entity/Categories";
+import { _makeGenericRouter } from "./_makeRouter";
 
 export interface ICatBody {
   userId: number;
@@ -18,54 +17,6 @@ const catController = new GenericController<
   ICatBodyPatch
 >(Categories);
 
-router
-  .post("/", async function(req, res, next) {
-    const { ...catFields }: ICatBody = req.body;
-    try {
-      const newCat = await catController.create(catFields);
-      return res.json(newCat);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .get("/:userId", async function(req, res, next) {
-    try {
-      let allCats = await catController.getAll(req.params.userId);
-      return res.json(allCats);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .get("/:userId/:id", async function(req, res, next) {
-    try {
-      const cat = await catController.get(req.params.userId, req.params.id);
-      return res.json(cat);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .delete("/:userId/:id", async function(req, res, next) {
-    try {
-      await catController.delete(req.params.userId, req.params.id);
-      return res.send(`Deleted task:${req.params.id}`);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .patch("/:userId/:id", async function(req, res, next) {
-    // cut out userId field
-    const { userId, ...rest } = req.body;
-    const catFields: ICatBodyPatch = rest;
-    try {
-      let task = await catController.update(
-        req.params.userId,
-        req.params.id,
-        catFields
-      );
-      return res.json(task);
-    } catch (err) {
-      return next(err);
-    }
-  });
-
-export default router;
+export default _makeGenericRouter<Categories, ICatBody, ICatBodyPatch>(
+  catController
+);
--- src/router/faveTask.ts
+++ src/router/faveTask.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import { FaveTask } from "../entity/FaveTask";
 import { GenericController } from "../controller/GenericController";
-
-const router = express.Router();
+import { _makeGenericRouter } from "./_makeRouter";
 
 export interface ITaskBody {
   userId: number;
@@ -14,58 +13,11 @@ interface ITaskBodyPatch {
   categoryId?: number;
 }
 
-const taskController = new GenericController<FaveTask, ITaskBody, ITaskBodyPatch>(
-  FaveTask
+const taskController = new GenericController<
+  FaveTask,
+  ITaskBody,
+  ITaskBodyPatch
+>(FaveTask);
+export default _makeGenericRouter<FaveTask, ITaskBody, ITaskBodyPatch>(
+  taskController
 );
-
-router
-  .post("/", async function(req, res, next) {
-    const { ...taskFields }: ITaskBody = req.body;
-    try {
-      const newTask = await taskController.create(taskFields);
-      return res.json(newTask);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .get("/:userId", async function(req, res, next) {
-    try {
-      let allTasks = await taskController.getAll(req.params.userId);
-      return res.json(allTasks);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .get("/:userId/:id", async function(req, res, next) {
-    try {
-      const task = await taskController.get(req.params.userId, req.params.id);
-      return res.json(task);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .delete("/:userId/:id", async function(req, res, next) {
-    try {
-      await taskController.delete(req.params.userId, req.params.id);
-      return res.send(`Deleted task:${req.params.id}`);
-    } catch (err) {
-      return next(err);
-    }
-  })
-  .patch("/:userId/:id", async function(req, res, next) {
-    // cut out userId field
-    const { userId, ...rest } = req.body;
-    const taskFields: ITaskBodyPatch = rest;
-    try {
-      let task = await taskController.update(
-        req.params.userId,
-        req.params.id,
-        taskFields
-      );
-      return res.json(task);
-    } catch (err) {
-      return next(err);
-    }
-  });
-
-export default router;
